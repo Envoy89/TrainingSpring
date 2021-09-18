75463db3b59cfa86572d2932df388f33d7e8927b
Minyazev Ruzil <ruzil33@gmail.com>
2021-09-18 14:23:26 +0300

    init simple crud app

---
+++ SimpleCrud/src/test/java/spring/training/SimpleCrud/SimpleCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.training.SimpleCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
